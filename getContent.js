7a5b6d6c20430c4fa2e7e3743e1b11a7c30ebe91
Ahmad <ahmadirfan746@gmail.com>
2023-11-18 02:52:36 +0500

    2nd try

---
+++ getContent.js
@@ -0,0 +1,22 @@
+// getContent.js
+
+const githubUsername = 'ahmad1101';
+const repoName = 'content-writing-portfolio';
+
+exports.handler = async function (event, context) {
+    try {
+        const response = await fetch(`https://github.com/${githubUsername}/${repoName}/blob/main/${event.queryStringParameters.file}`);
+        const content = await response.text();
+
+        return {
+            statusCode: 200,
+            body: content,
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
+};
+

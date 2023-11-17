c598b19c9a919cf2f7ed5b5ddc3d5e65a2e81f2a
Ahmad <ahmadirfan746@gmail.com>
2023-11-18 02:20:53 +0500

    Adding rest of the assets, html, css and js pages

---
+++ getContent.js
@@ -0,0 +1,18 @@
+// getContent.js
+
+exports.handler = async function (event, context) {
+    try {
+        const response = await fetch(`https://raw.githubusercontent.com/${githubUsername}/${repoName}/main/${event.queryStringParameters.file}`);
+        const content = await response.text();
+
+        return {
+            statusCode: 200,
+            body: content,
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+
+let scrollingDiv = document.querySelector('.blog-scroll');
+
+
+let tileCreator = (title, keyword) => {
+    let blogTile = document.createElement('div');
+    blogTile.classList.add("blog-tile");
+    
+    let titleDiv = document.createElement('div');
+    titleDiv.classList.add("title-div");
+    
+    let titleName = document.createElement('h3');
+    titleName.innerHTML = title;
+    titleDiv.appendChild(titleName);
+    blogTile.appendChild(titleDiv);
+    
+    let buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('button-div');
+    let readButton = document.createElement('button');
+    readButton.classList.add('send-button');
+    readButton.innerHTML = 'Read more';
+    buttonDiv.appendChild(readButton);
+    blogTile.appendChild(buttonDiv);
+    scrollingDiv.appendChild(blogTile);
+
+
+    let filePath = keyword;
+    // readButton.addEventListener('click', () => {
+    //     const fs = require("fs");
+
+    //     fs.open(`content-files/${filePath}.docx`, "r", (err, file) => {
+    //     if (err) throw err;
+    //     console.log(file);
+    //     });
+    // });
+
+    readButton.addEventListener('click', async () => {
+        try {
+            const response = await fetch('/.netlify/functions/getContent?file=' + keyword + '.docx');
+            // Handle the response
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    });
+
+}
+
+// tileCreator("The advantages of steel fabrication in construction", "steel-fabrication");
+tileCreator("Google UAC: Why you need it for apps?", "google-uac");
